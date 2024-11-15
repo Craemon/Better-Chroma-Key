15277d68ac36c7773fd1a9c1a7b9dc8137bf645f
Craemon <121398546+Craemon@users.noreply.github.com>
2024-11-16 00:45:04 +0100

    1.21.3 final (:pray:)

--- src/main/java/com/craemon/betterchromakey/BetterChromaKeyClient.java
+++ src/main/java/com/craemon/betterchromakey/BetterChromaKeyClient.java
@@ -1,10 +1,11 @@
 package com.craemon.betterchromakey;
 
+import com.craemon.betterchromakey.item.ModItemGroups;
 import net.fabricmc.api.ClientModInitializer;
 
 public class BetterChromaKeyClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ModItemGroups.registerItemGroups();
     }
 }
